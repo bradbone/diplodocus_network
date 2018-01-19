7b46bab8bd30a36d67e8b0ea8d37c03fe9f5e607
Brad <dev@bradbone.com>
2018-01-19 21:06:31 +1030

    added network_scripts folder

---
+++ management_scripts/game_saves_backup.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# Script to copy game save files to a specific location on local drive sdb. 
+# Files will then periodically shifted to network and offline backup.
+
+# Borderlands 2
+# cp -vR ~/.local/share/aspyr-media/borderlands\ 2/willowgame/savedata/ ~/Downloads/backup/Borderlands\ 2/
+
+# Layers of Fear
+# cp -vR ~/.config/unity3d/Bloober\ Team/Layers\ of\ Fear/0/ ~/Downloads/backup/Layers\ of\ Fear/
+
+# Hand of Fate
+# cp -vR /home/brad/.config/unity3d/Defiant\ Development/Hand\ of\ Fate/ ~/Downloads/backup/Hand\ of\ Fate/
+
+# Darkest Dungeon
+# cp -vR ~/.local/share/Red\ Hook\ Studios/Darkest ~/Downloads/backup/Darkest\ Dungeon/
+
+# Torchlight 2
+# cp -vR ~/.local/share/Steam/steamapps/common/Torchlight\ II/my\ games/runic\ games/torchlight\ 2 ~/Downloads/backup/Torchlight\ 2/
+
+# Costume Quest
+# cp -vR /home/brad/.local/share/Steam/userdata/22777368/115100/remote ~/Downloads/backup/Costume\ Quest/
+
+# Stardew Valley
+# cp -vR /home/brad/.config/StardewValley/Saves/Brad_140877143 /home/brad/triceratops/backup/Game\ Saves/Stardew\ Valley/
+
+# Shadow Warrior
+#cp -vR /home/brad/games/steam/steamapps/common/Shadow\ Warrior/shadow\ warrior /home/brad/triceratops/backup/Game\ Saves/Shadow\ Warrior\ \(2013\)/
+
+# Space Pirates and Zombies
+cp -vR /home/brad/.local/share/spacepiratesandzombies /home/brad/triceratops/backup/Game\ Saves/spaz/
+
+# Pillars of Eternity
+cp -vR /home/brad/.local/share/PillarsOfEternity/SavedGames /home/brad/triceratops/backup/Game\ Saves/Pillars\ of\ Eternity/
+
+# XCOM Enemy Unknown
+# cp -vR /home/brad/.local/share/feral-interactive/XCOM/SaveData /home/brad/triceratops/backup/Game\ Saves/X-COM\ Enemy\ Unknown/
+
+# XCOM Enemy Within
+# cp -vR /home/brad/.local/share/feral-interactive/XCOM/XEW/SaveData /home/brad/triceratops/backup/Game\ Saves/X-COM\ Enemy\ Within/
+
+# Mad Max
+# cp -vR /home/brad/.local/share/Steam/userdata/22777368/234140/remote/ /home/brad/triceratops/backup/Game\ Saves/Mad\ Max/
+
+# Cities Skylines
+cp -vR /home/brad/.local/share/Colossal\ Order/Cities_Skylines/Saves/ /home/brad/triceratops/backup/Game\ Saves/Cities\ Skylines/
+
+# Solar 2
+#cp -vR /home/brad/.local/share/Steam/userdata/22777368/97000 /home/brad/triceratops/backup/Game\ Saves/Solar\ 2/
+
+# World of Goo
+cp -vR /home/brad/.WorldOfGoo/ /home/brad/triceratops/backup/Game\ Saves/World\ of\ Goo/
+
+# Ziggurat
+#cp -vR /home/brad/.local/share/Steam/userdata/22777368/308420/remote/ /home/brad/triceratops/backup/Game\ Saves/Ziggurat/
+
+# Gone Home
+# cp -vR /home/brad/.config/unity3d/The\ Fullbright\ Company/Gone\ Home/Saves/ /home/brad/triceratops/backup/Game\ Saves/Gone\ Home/
+
+# Firewatch
+# cp -vR /home/brad/.config/unity3d/CampoSanto/Firewatch/saves/ /home/brad/triceratops/backup/Game\ Saves/Firewatch/
+
+# Steamworld Dig
+cp -vR /home/brad/.local/share/SteamWorldDig/ /home/brad/triceratops/backup/Game\ Saves/Steamwordl\ Dig/
+
+# A Story About My Uncle
+# cp -vR /home/brad/games/steam/steamapps/common/A\ Story\ About\ My\ Uncle/ASAMU/Saves /home/brad/triceratops/backup/Game\ Saves/A\ Story\ About\ My\ Uncle/
+
+# The Swapper
+# cp -vR /home/brad/.local/share/Facepalm\ Games/The\ Swapper\ 1000 /home/brad/triceratops/backup/Game\ Saves/The\ Swapper/
+
+# Dawn of War II
+#cp -vR /home/brad/.local/share/feral-interactive/Dawn\ of\ War\ II/SaveData/ /home/brad/triceratops/backup/Game\ Saves/Dawn\ of\ War\ II/
+
+# The following game saves are currently residing on the attached Windows partition. 
+# This may need updating if the hard drive id ever changes
+
+# Lichdom Battlemage
+#cp -vR /run/media/brad/E0EE8E26EE8DF4D6/Users/bradb/Saved\ Games/Lichdom\ Battlemage/savegames /home/brad/triceratops/backup/Game\ Saves/Lichdom\ Battlemage/
+
+# Lords of the Fallen
+#cp -vR /run/media/brad/E0EE8E26EE8DF4D6/Games/Steam/userdata/22777368/265300/remote/ /home/brad/triceratops/backup/Game\ Saves/Lords\ of\ the\ Fallen/
+
+# The Solus Project
+#cp -vR /run/media/brad/E0EE8E26EE8DF4D6/Users/bradb/Documents/TheSolusProject/SolusProfile/ /home/brad/triceratops/backup/Game\ Saves/The\ Solus\ Project/
+
+# Hitman (2016)
+cp -vR /run/media/brad/E0EE8E26EE8DF4D6/Games/Steam/userdata/22777368/236870/ /home/brad/triceratops/backup/Game\ Saves/Hitman/
+
+# Resident Evil 7
+#cp -vR /run/media/brad/E0EE8E26EE8DF4D6/Games/Steam/userdata/22777368/418370/remote/win64_save/ /home/brad/triceratops/backup/Game\ Saves/Resident\ Evil\ 7/
+
+# Call of Duty Black Ops
+#cp -vR /run/media/brad/E0EE8E26EE8DF4D6/Games/Steam/steamapps/common/Call\ of\ Duty\ Black\ Ops/players/save/ /home/brad/triceratops/backup/Game\ Saves/Call\ of\ Duty\ Black\ Ops/
+
+# Mass Effect 2
+#cp -vR /run/media/brad/E0EE8E26EE8DF4D6/Users/bradb/Documents/BioWare/Mass\ Effect\ 2/Save/ /home/brad/triceratops/backup/Game\ Saves/Mass\ Effect\ 2/
+
+# SWAT 3 Close Quarters Battle
+# cp -vR /run/media/brad/E0EE8E26EE8DF4D6/Games/SWAT\ 3/careers/ /home/brad/triceratops/backup/Game\ Saves/SWAT\ 3/
